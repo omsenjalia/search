d6faefcdebf4ba20ff464bc436425f7c5fb712cb
om <omsenjalia@gmail.com>
2024-04-30 15:08:53 +0530

    Create e.sh

---
+++ e.sh
@@ -0,0 +1,2 @@
+wget https://gemmei.ftp.acc.umu.se/debian-cd/current/amd64/iso-cd/debian-12.5.0-amd64-netinst.iso
+wget https://github.com/balena-io/balena-cli/releases/download/v18.2.1/balena-cli-v18.2.1-linux-x64-standalone.zip
