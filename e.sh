dccd2d073c7acff22ae9ec94d4ffb74121496f73
om <omsenjalia@gmail.com>
2024-05-01 12:04:04 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,2 +1,3 @@
-wget https://gemmei.ftp.acc.umu.se/debian-cd/current/amd64/iso-cd/debian-12.5.0-amd64-netinst.iso
+wget https://download.sys.truenas.net/TrueNAS-SCALE-Dragonfish/24.04.0/TrueNAS-SCALE-24.04.0.iso
 wget https://github.com/balena-io/balena-cli/releases/download/v18.2.1/balena-cli-v18.2.1-linux-x64-standalone.zip
+unzip balena-cli-v18.2.1-linux-x64-standalone.zip
