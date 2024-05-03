6d7cf1307b833ef60c1e2a098d011a265fb6d87b
om <omsenjalia@gmail.com>
2024-05-03 15:57:17 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,3 +1,6 @@
-wget https://download.sys.truenas.net/TrueNAS-SCALE-Dragonfish/24.04.0/TrueNAS-SCALE-24.04.0.iso
-wget https://github.com/balena-io/balena-cli/releases/download/v18.2.1/balena-cli-v18.2.1-linux-x64-standalone.zip
-unzip balena-cli-v18.2.1-linux-x64-standalone.zip
+cd /mnt/DataShare/Ventoy
+wget -O debain-12.5.0.iso https://gemmei.ftp.acc.umu.se/debian-cd/current/amd64/iso-cd/debian-12.5.0-amd64-netinst.iso
+wget -O umbrel.img.xz https://objects.githubusercontent.com/github-production-release-asset-2e65be/277621205/13b57334-7546-44f5-855b-ad7b63e02d32?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240503%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240503T101927Z&X-Amz-Expires=300&X-Amz-Signature=b5c14ca018cb3f8fb572bf73c4be3a30ed9b27ff26d910ca93d6c44d19309555&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=277621205&response-content-disposition=attachment%3B%20filename%3Dumbrelos-amd64-usb-installer.img.xz&response-content-type=application%2Foctet-stream
+wget -O ubuntu.24.04.iso https://releases.ubuntu.com/24.04/ubuntu-24.04-live-server-amd64.iso
+wget -O ventoy.iso https://excellmedia.dl.sourceforge.net/project/ventoy/v1.0.97/ventoy-1.0.97-livecd.iso?viasf=1
+wget -O kali.24.1.iso https://kali.download/base-images/kali-2024.1/kali-linux-2024.1-installer-amd64.iso
